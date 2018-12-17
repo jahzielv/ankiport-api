2a2d92a7b6028e3ee9137f233cbdc2cfff410f05
Jahziel Villasana <jahzielv@gmail.com>
2018-12-17 17:00:55 -0500

    added CORS management

--- server.py
+++ server.py
@@ -5,8 +5,10 @@ import os
 import ankiport_core.quizlet_helper as qh
 from flask import send_file
 from flask import send_from_directory
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/")
@@ -51,7 +53,11 @@ def portQ():
         response.headers["x-filename"] = portResults[1] + ".apkg"
 
         response.headers["Access-Control-Expose-Headers"] = 'x-filename'
-        # "<h3>Successfully ported set {}</h3>".format(portResults[1])
+
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization"
+        response.headers["Access-Control-Allow-Methods"] = "GET,HEAD,OPTIONS,POST,PUT"
+    # "<h3>Successfully ported set {}</h3>".format(portResults[1])
         return response
     else:
         return "<h3>Port failed :(</h3>"
